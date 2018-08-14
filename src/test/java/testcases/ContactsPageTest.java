0cae5f7028fd52114a57581c8d1a2a3ee8bbb3fe
pradeep190686 <pradeep190686@gmail.com>
2018-08-14 17:35:08 +0530

    expectedexceptions on ContactsPage

--- src/test/java/testcases/ContactsPageTest.java
+++ src/test/java/testcases/ContactsPageTest.java
@@ -2,13 +2,15 @@ package testcases;
 
 import java.io.IOException;
 
+import javax.naming.NameNotFoundException;
+
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.openqa.selenium.NoSuchElementException;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import pages.ContactsPage;
 import pages.HomePage;
 import pages.LoginPage;
@@ -16,17 +18,17 @@ import testbase.TestBase;
 import utilities.testutil;
 
 public class ContactsPageTest extends TestBase {
- testutil tutil;
- LoginPage loginpage;
- ContactsPage contactspage;
- HomePage homepage;
- String sheetName="Sheet1";
-	
+	testutil tutil;
+	LoginPage loginpage;
+	ContactsPage contactspage;
+	HomePage homepage;
+	String sheetName="Sheet1";
+
 	public ContactsPageTest()
 	{
 		super();
 	}
-	
+
 	@BeforeMethod
 	public void setup()
 	{
@@ -37,45 +39,45 @@ public class ContactsPageTest extends TestBase {
 		homepage=loginpage.login(prop.getProperty("username"),prop.getProperty("password"));
 		tutil.switchtoframe();
 		contactspage=homepage.ClickOnContactsLink();
-		
+
 	}
-	
+
 	@Test(priority=1)
 	public void VerifyContactLabelDisplayTest()
 	{
 		log.info("contact label is displayed");
 		Assert.assertTrue(contactspage.VerifyContactsLabel(),"contacts label not found");		
 	}
-	
-	@Test(priority=2,enabled = true)
+
+	@Test(priority=2,enabled = true,expectedExceptions=NoSuchElementException.class)
 	public void checkboxtest()
 	{
 		contactspage.selectContact("chiran amgai");
 		log.debug("check box not selected");
 	}
-	
+
 	@Test(priority=3,enabled = false)
 	public void multicheckboxtest()
 	{
 		contactspage.selectContact("chiran amgai");
 		contactspage.selectContact("Binu Amgai");
-		
+
 	}
-	
+
 	@Test(priority=4)
 	public void CreateNewContactTest()
 	{
 		homepage.ClickOnNewContactLink();
 		contactspage.CreateNewContact("Mr.","Tom","Jerry","Elle");
 	}
-	
+
 	@DataProvider
 	public Object[][] getexcelTestData() throws Exception, IOException
 	{
 		Object[][] data1=tutil.getTestData(sheetName);	
 		return data1;
 	}
-	
+
 	@Test(priority=5,dataProvider="getexcelTestData")
 	public void CreateNewContactExcelTest(String title,String firstname,String lastname,String company)
 	{
@@ -83,7 +85,7 @@ public class ContactsPageTest extends TestBase {
 		//contactspage.CreateNewContact("Mr.","Tom","Jerry","Elle");
 		contactspage.CreateNewContact(title, firstname, lastname,company);
 	}
-	
+
 	@AfterMethod
 	public void teardown()
 	{
